6dd387a69ac11aa4507dc4c797bfce9bc21b5243
fivedoor <jh5dl@yahoo.co.uk>
2020-05-05 15:26:20 +0100

    Version 0.0.1 - Added event-thumbnail

---
+++ src/app/event-thumbnail/event-thumbnail.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EventThumnailComponent } from './event-thumnail.component';
+
+describe('EventThumnailComponent', () => {
+  let component: EventThumnailComponent;
+  let fixture: ComponentFixture<EventThumnailComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EventThumnailComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EventThumnailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
