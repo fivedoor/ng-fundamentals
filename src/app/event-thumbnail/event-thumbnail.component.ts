9bb27d115fa6a10835ad3f93a1ba5f72e97923ba
fivedoor <jh5dl@yahoo.co.uk>
2020-05-06 15:28:14 +0100

    Version 0.0.2 - Add Nav bar component - style updates to thumbnail

--- src/app/event-thumbnail/event-thumbnail.component.ts
+++ src/app/event-thumbnail/event-thumbnail.component.ts
@@ -8,11 +8,25 @@ import { Component, OnInit, Input } from '@angular/core';
 export class EventThumbnailComponent implements OnInit {
   @Input() event:any
 
+
+// set to any because we're returning a 
   getStartTimeStyle():any {
     if (this.event && this.event.time === '8:00 am')
       return {color: '#003300', 'font-weight': 'bold'}
     return {}
   }
+
+  // ngClass can return an object 
+  // or a string  which is space separated lis of classes 
+  // or you can retrun an array of strings
+  // First return applies if the statement is true ‘otherwise return an empty string
+
+  getStartWithNgClass() {
+    if (this.event && this.event.time === '10:00 am')
+      return 'italicise';
+      return '';
+  }
+
 
   constructor() { }
 
